c60c8d81333d429a499ede792199cd179d29dea1
Pospelov_Vyacheslav_2121gr <S89182168881@yandex.ru>
2023-05-21 19:28:39 +0300

    release v2

--- DotaAppТNout.py
+++ DotaAppТNout.py
@@ -1,10 +1,14 @@
 import mouse
 import keyboard as keyb
 import time
-# keyb.wait('a')
-# c = mouse.get_position()
-# print(c)
 def main():
+    def control():
+        nonlocal IsWork
+        if IsWork:
+            IsWork = False
+            print("finish")
+        else:
+            IsWork = True
     try:
         respawn = int(input('Enter resp (1 - bottom / 2 - top)'))
     except:
@@ -13,19 +17,21 @@ def main():
     if respawn != 1 and respawn != 2:
         print('You can only enter 1 or 2')
         raise SystemExit(0)
-    position_throne = (195, 713) if respawn == 1 else (10, 888)
+    IsWork = False
+    position_throne = (192, 679) if respawn == 1 else (16, 846)
+    IsWork = False
+    keyb.add_hotkey('ctrl+shift+a', control)
     def move_mouse():
-        time.sleep(5)
+        time.sleep(1)
         start_position = mouse.get_position()
         mouse.move(position_throne[0], position_throne[1], duration=0.5)
         time.sleep(1)
         mouse.click('right')
         time.sleep(1)
         mouse.move(start_position[0], start_position[1], duration=0.5)
-    keyb.wait('ctrl+shift+a')
     while True:
-        move_mouse()
-        if keyb.is_pressed('ctrl+shift+a') == True:
-           break
+        if keyb.is_pressed("ctrl+shift+a"):
+            while IsWork:
+                move_mouse()
 if __name__ == '__main__':
     main()
